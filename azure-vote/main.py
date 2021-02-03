cc6138be54857903a1f1e9d17ae811d22bde49ec
Todd Nguyen <t0dd2000@yahoo.com>
2021-02-02 21:32:59 -0500

    test

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -99,12 +99,12 @@ def index():
         vote1 = r.get(button1).decode('utf-8')
 
         # TODO: use tracer object to trace cat vote
-        tracer.span(name="Cat Vote")
+        #tracer.span(name="Cat Vote")
 
         vote2 = r.get(button2).decode('utf-8')
 
         # TODO: use tracer object to trace dog vote
-        tracer.span(name="Dog Vote")
+        #tracer.span(name="Dog Vote")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -137,7 +137,12 @@ def index():
             vote = request.form['vote']
             r.incr(vote,1)
             logger.warning('Voted for ' + vote)
-            
+            if(vote=="Dogs"):
+                logging.warnings("vote = Dogs - set span to Dogs")
+                tracer.span(name="Voted for Dogs")
+            else:
+                logging.warnings("vote = Cats - set span to Cats")
+                tracer.span(name="Voted for Cats")
 
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
